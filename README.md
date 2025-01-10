b9aceadbaa57acce7663cc7031e1c3a8524e43d0
Chet Husk <chusk3@gmail.com>
2025-01-10 11:09:01 -0600

    test and README

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # check-dotnet-install-scripts-action
-A Github Actions script that checks if a local copy of the dotnet-install scripts is up to date or not
+
+A Github Actions script that checks if a local copy of the dotnet-install scripts is up to date or not, according to the official dotnet CDN.
+This script uses the [ETag](https://developer.mozilla.org/docs/Web/HTTP/Headers/ETag), or Entity Tag, value of the script to determine up-to-date-d-ness.
+
+## Inputs
+
+### `script-file`
+
+**Required** The name of the script file to check. Should be either 'dotnet-install.sh' or 'dotnet-install.ps1'. Defaults to 'dotnet-install.sh'
+
+### `etag`
+
+**Required** The ETag value of the script to check against. This is a string value that can be found in the 'ETag' HTTP response headers of the script URL. There is no default, so you will need to store and know this value. 
+
+You can get the latest one by running the following command in your terminal:
+
+```bash
+curl -s --head https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.sh | grep ETag | cut -d' ' -f2
+```
+
+```powershell
+(curl -s --head https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.ps1 | select-string ETag) -split " " | select-object -index 1
+```
+
+## Outputs
+
+### `current-etag`
+
+The current ETag of the script that was requested. This is the value that you can use to update your local copy of the script.
+
+## Example usage
+
+```yaml
+uses: actions/hello-world-javascript-action@e76147da8e5c81eaf017dede5645551d4b94427b
+with:
+    script-file: dotnet-install.sh
+    etag: 0x8DD2F3D8D65F264
+```
--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const core = require('@actions/core');
-
+const httpClient = require('@actions/http-client');
 const dotnet_cdn_domain = "https://builds.dotnet.microsoft.com";
 const dotnet_script_path = "dotnet/scripts/v1";
+const version = require('./package.json').version;
+const util = require('util');
 
-try {
-    const script_file = core.getInput('script-file');
-    const etag = core.getInput('etag');
+async function run() {
+    try {
+        const script_file = core.getInput('script-file', { required: true });
+        const user_etag = core.getInput('etag', { required: true });
 
-    const script_url = new URL(`${dotnet_cdn_domain}/${dotnet_script_path}/${script_file}`);
-    const check_response = await fetch({
-        url: script_url,
-        method: HEAD,
-        headers: { "If-None-Match": etag }
-    });
-
-    if (check_response.status === 304) {
-        // it's a match - set the etag and succeed
-        core.setOutput("current-etag", etag);
-        return;
+        const script_url = new URL(`${dotnet_cdn_domain}/${dotnet_script_path}/${script_file}`);
+        const client = new httpClient.HttpClient(`actions/check-dotnet-install-scripts-action@${version}`);
+        const { message } = await client.head(script_url.toString(), { "If-None-Match": user_etag });
+        if (core.isDebug) {
+            core.debug(`Received ${message.statusCode} from HEAD request to ${script_url.toString()}.`);
+            core.debug(`Received the following headers:`);
+            core.debug(message.headers);
+        }
+        if (message.statusCode === 304 || (message.statusCode === 200 && message.headers["etag"] === user_etag)) {
+            // it's a match - set the etag and succeed
+            core.setOutput("current-etag", user_etag);
+            console.log(`No updates available for ${script_file}.`);
+            return;
+        }
+        else if (message.statusCode === 200) {
+            // not an ETag match, send the 'current' ETag and fail
+            const current_etag = message.headers["etag"];
+            core.setOutput("current-etag", current_etag);
+            core.setFailed(`There is a more recent version of ${script_file} available with ETag ${current_etag}. Please update your repository to use the latest version.`);
+            return;
+        }
+        else {
+            // there was some kind of error, fail the action
+            core.setFailed(`Error checking for updates to ${script_file}: ${message.statusCode}}`);
+        }
+    } catch (error) {
+        core.setFailed(error.message);
     }
-    else if (check_response.status === 200) {
-        // not an ETag match, send the 'current' ETag and fail
-        const current_etag = check_response.headers.get("ETag");
-        core.setOutput("current-etag", current_etag);
-        core.setFailed(`There is a more recent version of ${script_file} available with ETag ${current_etag}. Please update your repository to use the latest version.`);
-        return;
-    }
-    else {
-        // there was some kind of error, fail the action
-        core.setFailed(`Error checking for updates to ${script_file}: ${check_response.status} ${check_response.statusText}`);
-    }
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+run()
